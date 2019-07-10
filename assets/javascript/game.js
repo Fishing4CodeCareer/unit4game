bd26984ade95b45800e443203eb72864459dde61
Allan Pearson <Alprsn2012@gmail.com>
2019-07-09 23:56:45 -0500

    mage tags added to button links in

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,50 @@
-var numbers = ["1", "5", "15", "25"]
-    , randomNumbers = ""
-    , winCount = 0
-    , lossCount = 0
-    , guessesLeft = 0
-    , guessedNumbers = []
-    , defaultGuessesLeft = 7;
+var targetNumber = 75;
 
-window.addEventListener(clickon.button, onButtonPress, false);
-newGame();
-var sum
-function newGame() {
-    guessedNumbers = [];
-    resetGuesses();
-    generateRandomNumber();
-    displayOnScreen("wins", "wins: " + winCount);
-    displayOnScreen("losses", "losses: " + lossCount);
-    displayOnScreen("guessesleft", "Guesses Left: " + guessesLeft);
-}
-function clickon() {
-    number = clickon.button();
+  $("#number-to-guess").text(targetNumber);
 
-    if (numbers.includes(number, 0) && guessedNumbers.includes(number, 0) === false) {
-        guessedNumbers.push(clickon.button);
-        printKeyPressed(number);
-        guessesLeft--;
-        displayOnScreen("guessesleft", "Guesses Left: " + guessesLeft);
-        if (String(number) == String(randomNumber)) {
-            winCount++;
-            displayOnScreen("wins", "wins: " + winCount);
-            newGame();
-        }
-        if (guessesLeft === 1) {
-            lossCount++;
-            displayOnScreen("losses", "losses: " + lossCount);
-            newGame();
-        }
+  var counter = 0;
+
+  var numberOptions = [10, 5, 3, 7];
+
+  
+  for (var i = 0; i < numberOptions.length; i++) {
+
+   
+    var imageCrystal = $("<img>");
+
+    
+    imageCrystal.addClass("crystal-image");
+
+    
+    imageCrystal.attr("src", "assets/images/box.gif");
+    imageCrystal.attr("src", "assets/images/pir.gif");
+    imageCrystal.attr("src", "assets/images/spin.gif");
+    imageCrystal.attr("src", "assets/images/box.gif");
+    imageCrystal.attr("src", "assets/images/box.gif");
+      <img src="assets/images/box.gif" class="auth-image" alt="box" button id="box.button" value="25">
+      <img src="assets/images/pir.gif" class="auth-image" alt="pir" button id="pir.button" value="1">
+      <img src="assets/images/spin.gif" class="auth-image" alt="spin" button id="spin.button" value="5">
+      <img src="assets/images/sun.gif" class="auth-image" alt="sun" button id="sun.button" value="15"></img>
+    
+    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+    
+    $("#crystals").append(imageCrystal);
+  }
+
+  $(".crystal-image").on("click", function() {
+
+    var crystalValue = ($(this).attr("data-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    
+    counter += crystalValue;
+
+    alert("New score: " + counter);
+    if (counter === targetNumber) {
+      alert("You win!");
     }
-}
-
-function generateRandomNumber() {
-    randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
-}
-function printKeyPressed(numbers) {
-    if (guessesLeft == 7) {
-        document.getElementById("guessessofar").innerHTML += number;
-    } else {
-        document.getElementById("guessessofar").innerHTML += ", " + number;
+    else if (counter >= targetNumber) {
+      alert("You lose!!");
     }
-}
 
-function resetGuesses() {
-    guessesLeft = defaultGuessesLeft;
-    displayOnScreen("guessesleft", "Losses: " + guessesLeft);
-    displayOnScreen("guessessofar", "Your Guesses so far: ");
-}
-
-function displayOnScreen(elementId, content) {
-    document.getElementById(elementId).innerHTML = content;
-}
+  });
